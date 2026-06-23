50e21b668b438148c89a3cec8ad54dd649c811d9
Harsh Bansal <96734004+Harshbansal06@users.noreply.github.com>
2026-06-23 14:55:46 +0530

    Login page is ready
    i have made login page, updated the variables in tailwind css, installed modules, intall lucide-react for icons

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,16 +2,20 @@ import React from 'react';
 import Sidebar from './Components/sidebar';
 import SearchBar from './Components/searchBar';
 import NotificationComponent from './Components/notificationComponent';
+import LoginPage from './Components/loginPage';
 
 function App() {
   return (
-    <div className="flex w-screen h-screen overflow-hidden"> 
-      <Sidebar />
-     <div className="flex-1 flex flex-col items-start p-6 bg-[#0a0a0a] overflow-y-auto">
-        <SearchBar />
-           
-      </div>
-    </div>
+    <>
+      <LoginPage />
+      {/* <div className="flex w-screen h-screen overflow-hidden">
+        <Sidebar />
+        <div className="flex-1 flex flex-col items-start p-6 bg-[#0a0a0a] overflow-y-auto">
+          <SearchBar />
+
+        </div>
+      </div> */}
+    </>
   );
 }
 export default App
--- frontend/src/Components/loginPage.jsx
+++ frontend/src/Components/loginPage.jsx
@@ -1,8 +1,69 @@
-function LoginPage() 
-{
+import Login from '../assets/Site Assets/login_img.png'
+import Logo from "../assets/Site Assets/Logo.png"
+import { User, Lock } from "lucide-react";
+
+
+function LoginPage() {
   return (
     <>
-    
+      <div className=" min-h-screen flex w-full font-mono">
+        <div className="relative flex flex-col w-[51.5%] min-h-screen overflow-hidden min-w-150">
+          <img
+            src={Login}
+            className="absolute inset-0 w-full h-full object-cover pointer-events-none"
+          />
+
+
+        </div>
+
+
+        <div className='relative bg-bg flex justify-center items-center w-full min-h-screen px-10 '>
+          <div className='flex flex-col gap-6 my-14'>
+            <div className='flex items-center flex-col gap-2.5'>
+              <img src={Logo} alt="" srcset="" className='w-17' />
+              <h1 className='font-orbitron font-bold text-3xl text-heading '>ERS Hive</h1>
+              <p className='text-[16px] font-mono text-fg'>Inventory Mangement Service</p>
+            </div>
+
+            {/* Form section */}
+            <div className='font-rajdhani bg-surface border border-border rounded-xl  w-115 p-9 shadow-[0_0_50px_5px] shadow-circuit'>
+              <form action="" className='m-auto'>
+
+                <label htmlFor="" className='text-gold block mb-2'>Roll Number</label>
+                <div className='bg-circuit rounded-sm flex px-5 py-2 items-center gap-2.5 mb-5'>
+                  <User size={16} style={{ color: 'gold', flexShrink: 0 }} />
+                  <input type="text" placeholder='Enter your roll number' className='text-fg w-full outline-none' />
+                </div>
+
+                <label htmlFor="" className='text-gold block mb-2'>Password</label>
+                <div className='bg-circuit rounded-sm flex px-5 py-1.5 items-center gap-2.5 mb-6'>
+                  <Lock size={16} style={{ color: 'gold', flexShrink: 0 }} />
+                  <input type="password" placeholder='Enter your password' className='text-fg w-full outline-none' />
+                </div>
+
+
+                <button type='submit' className='bg-gold font-orbitron w-full py-2 rounded-md font-bold mb-7'>
+                  Login
+                </button>
+                <div className="flex justify-center">
+                  <button
+                    type="button"
+                    className='font-mono text-fg '
+                  >
+                    Forgot Password?
+                  </button>
+                </div>
+
+
+              </form>
+            </div>
+            <p className='text-mono mt-5  text-gold text-center' >
+              Electronics &amp; Robotics Society • IIITDM Jabalpur
+            </p>
+          </div>
+        </div>
+
+      </div>
     </>
   )
 }
