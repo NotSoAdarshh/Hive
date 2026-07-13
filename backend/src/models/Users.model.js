699139abbe99c5156fe262304deff3d2ac45656d
mohitjaint <imohitjaint@gmail.com>
2026-07-13 17:04:53 +0530

    feat: add ApiError and ApiResponse classes for standardized error handling feat: implement asyncHandler utility for handling asynchronous requests

--- backend/src/models/Users.model.js
+++ backend/src/models/Users.model.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema(
   {},
   {
-    collection: "user", // This is the important part
+    collection: "user",
     strict: false,
   }
 );
